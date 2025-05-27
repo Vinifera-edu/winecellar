57308a5819750e18a3771b88b3e87ed4e4d22bdb
Vinifera-edu <rafal.kujawa87@gmail.com>
2025-05-27 08:22:55 +0200

    Update create-admin.py

--- wine/views.py
+++ wine/views.py
@@ -16,6 +16,13 @@ from datetime import datetime
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 
+def create_admin(request):
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@example.com', 'admin1234')
+        return HttpResponse("Admin user created!")
+    else:
+        return HttpResponse("Admin user already exists.")
+
 
 class WinesView(LoginRequiredMixin, generic.ListView):
     model = Wine
