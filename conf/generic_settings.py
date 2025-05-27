88dd0628055d55110c2c5465f6a6fea489eaeab9
Vinifera-edu <rafal.kujawa87@gmail.com>
2025-05-27 08:41:44 +0200

    Update data base generic_settings.py

--- conf/generic_settings.py
+++ conf/generic_settings.py
@@ -116,10 +116,12 @@ LOGOUT_REDIRECT_URL = '/'
 
 DEBUG = True
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+}
+
     }
 }
 
