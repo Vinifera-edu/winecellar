8ed750c252d18721123ba19c3f2594c811eebf01
Vinifera-edu <rafal.kujawa87@gmail.com>
2025-05-27 06:16:39 +0200

    Update views.py

--- wine/views.py
+++ wine/views.py
@@ -18,7 +18,7 @@ from django.http import HttpResponse
 
 def create_admin(request):
     if not User.objects.filter(username='admin').exists():
-        User.objects.create_superuser('admin', 'rafal.kujawa87@gmail.com', 'Vinifera1234')
+        User.objects.create_superuser('admin', 'shuriken.kai@gmail.com', 'Vinifera1234')
         return HttpResponse("Admin user created!")
     else:
         return HttpResponse("Admin user already exists.")
