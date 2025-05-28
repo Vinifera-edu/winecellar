6026a9fc8c7db4bf515cbf596f2534d3fa1d6216
Vinifera-edu <rafal.kujawa87@gmail.com>
2025-05-28 17:18:51 +0200

    Update generic_settings.py

--- conf/generic_settings.py
+++ conf/generic_settings.py
@@ -122,6 +122,11 @@ DATABASES = {
     'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
+import os
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # wine.Wine: (models.W042) Auto-created primary key used when not defining a primary key type, by default 'django.db.models.AutoField'.
 # HINT: Configure the DEFAULT_AUTO_FIELD setting or the AppConfig.default_auto_field attribute to point to a subclass of AutoField, e.g. 'django.db.models.BigAutoField'.
 DEFAULT_AUTO_FIELD='django.db.models.AutoField'
