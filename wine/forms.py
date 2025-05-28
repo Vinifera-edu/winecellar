093a1d5c0f9121ae3b37eb2b22a0c37836cda712
Vinifera-edu <rafal.kujawa87@gmail.com>
2025-05-28 17:20:34 +0200

    Update forms.py

--- wine/forms.py
+++ wine/forms.py
@@ -5,3 +5,11 @@ from django import forms
 class WineForm(forms.Form):
     winename = forms.CharField(label='Weinname prost', max_length=20)
     # purchase = forms.DateField(widget=DateInput)
+
+fields = ['winename', ..., 'nmbrbottles', 'wine_image']
+
+widgets = {
+    # ... inne
+    'wine_image': forms.FileInput(attrs={'class': "form-control"}),
+}
+
