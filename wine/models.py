e69acd07c0e8cdcb8ae439ad7c4502b796311f8d
Vinifera-edu <rafal.kujawa87@gmail.com>
2025-05-28 17:59:11 +0200

    Update new models.py

--- wine/models.py
+++ wine/models.py
@@ -1,16 +1,6 @@
 from django.db import models
 from django.urls import reverse
-from django import forms
-from django.forms import ModelForm
 from django.contrib.auth.models import User
-from datetime import datetime
-
-from django.db import models
-from django.contrib.auth.models import User
-
-class Wine(models.Model):
-    # ... inne pola
-    wine_image = models.ImageField(upload_to='wine_images/', null=True, blank=True)
 
 COUNTRY = [
     ('-', '-'),
@@ -41,21 +31,21 @@ class Wine(models.Model):
     owner = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
     winename = models.CharField(max_length=200)
     producer = models.CharField(max_length=200, blank=True)
-    grapes = models.CharField(max_length=200, blank=True,)
-    winetype = models.CharField(max_length=12, blank=True, choices=WINETYPE, default='-')
+    grapes = models.CharField(max_length=200, blank=True)
+    winetype = models.CharField(max_length=12, blank=True, choices=WINETYPE.items(), default='-')
     year = models.IntegerField(blank=True, null=True)
     country = models.CharField(max_length=12, blank=True, choices=COUNTRY, default='-')
     region = models.CharField(max_length=200, blank=True)
-    purchase = models.DateField(blank=True, null=True, auto_now=False, auto_now_add=False)
-    price = models.CharField(max_length=12, blank=True, default=0)
+    purchase = models.DateField(blank=True, null=True)
+    price = models.CharField(max_length=12, blank=True, default='0')
     dealer = models.CharField(max_length=200, blank=True)
     drinkfrom = models.IntegerField(blank=True, null=True)
     drinkto = models.IntegerField(blank=True, null=True)
     nmbrbottles = models.IntegerField(default=0)
     warehouse = models.CharField(max_length=200, blank=True)
     notes = models.CharField(max_length=400, blank=True)
+    wine_image = models.ImageField(upload_to='wine_images/', null=True, blank=True)
     editdate = models.DateField(auto_now=True)
-
 
     class Meta:
         ordering = ["drinkto", "country", "region", "year"]
@@ -64,52 +54,4 @@ class Wine(models.Model):
         return reverse('wine-detail', kwargs={'pk': self.pk})
 
     def __str__(self):
-        return self.text
-
-class WineForm(ModelForm):
-    class Meta:
-        model = Wine
-        current_year = datetime.now().year
-        fields = ['winename', 'producer', 'country', 'region', 'year', 'winetype', 'grapes', 'purchase', 'dealer',
-                  'price', 'drinkfrom', 'drinkto', 'warehouse', 'notes', 'nmbrbottles']
-        labels = {
-        'winename': 'Wine name',
-        'producer': 'Producer',
-        'winetype': 'Wine type',
-        'grapes': 'Grapes',
-        'year': 'Vintage',
-        'country': 'Country',
-        'region': 'Region',
-        'purchase': 'Purchase date',
-        'price': 'Price (~EUR)',
-        'dealer': 'Merchant',
-        'drinkfrom': 'Drinkable from',
-        'drinkto': 'Drinkable until',
-        'warehouse': 'Storage location',
-        'nmbrbottles': 'Number of bottles',
-    }
-
-
-        widgets = {
-            'winename': forms.TextInput(attrs={'class': "form-control"}),
-            'producer': forms.TextInput(attrs={'class': "form-control"}),
-            'grapes': forms.TextInput(attrs={'class': "form-control"}),
-            'winetype': forms.Select(attrs={'class': "form-control"}),
-            'year': forms.NumberInput(attrs={'class': "form-control", 'min': '1990', 'max': '2025'}),
-            'country': forms.Select(attrs={'class': "form-control"}),
-            'region': forms.TextInput(attrs={'class': "form-control"}),
-            'purchase': forms.DateInput(format=('%Y-%m-%d'), attrs={"type": "date", 'class': "form-control"}),
-            'price': forms.TextInput(attrs={'class': "form-control"}),
-            'dealer': forms.TextInput(attrs={'class': "form-control"}),
-            'notes': forms.Textarea(attrs={'class': "form-control", 'cols': 100, 'rows': 5}),
-            'drinkfrom': forms.NumberInput(attrs={'class': "form-control", 'min': '2000', 'max': '2060'}),
-            'drinkto': forms.NumberInput(attrs={'class': "form-control", 'min': '2018', 'max': '2060', 'type': 'number'}),
-            'warehouse': forms.TextInput(attrs={'class': "form-control"}),
-            'nmbrbottles': forms.NumberInput(attrs={'class': "form-range", 'type': 'range', 'min': '0', 'max': '18'}),
-        }
-fields = ['winename', ..., 'nmbrbottles', 'wine_image']
-
-widgets = {
-    # ... inne
-    'wine_image': forms.FileInput(attrs={'class': "form-control"}),
-}
+        return self.winename
